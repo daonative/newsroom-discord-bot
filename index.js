188eff75a5d494c31df1347b3b639bf1b0515519
Laurent De Marez <laurent@prologe.io>
2021-11-23 17:25:56 +0100

    feat: Default back to the newsroom discord server

--- index.js
+++ index.js
@@ -24,26 +24,36 @@ const getRoomGuildSettings = async (roomName) => {
     const roomData = roomDoc.data()
 
     if (!roomData?.discordGuildId || !roomData?.discordAnnouncementsChannelId || !roomData?.discordNewsroomCategoryChannelId) {
-      return
+      return null
     }
 
     return {
       guildId: roomData.discordGuildId,
       announcementsChannelId: roomData.discordAnnouncementsChannelId,
-      newsroomCategoryChannelId: roomData.discordNewsroomCategoryChannelId
+      newsroomCategoryChannelId: roomData.discordNewsroomCategoryChannelId,
+      prependRoomName: false
     }
   } catch (error) {
-    console.log(error)
     return null
   }
 }
 
 const onNewTask = async (task) => {
-  guildSettings = await getRoomGuildSettings(task.room)
+  let guildSettings = await getRoomGuildSettings(task.room)
+
+  // Use default guild settings (if it exists) when the room doesn't have guild settings
+  if (!guildSettings && process.env.DISCORD_DEFAULT_GUILD) {
+    guildSettings = {
+      guildId: process.env.DISCORD_DEFAULT_GUILD,
+      announcementsChannelId: process.env.DISCORD_DEFAULT_ANNOUCEMENTS_CHANNEL,
+      newsroomCategoryChannelId: process.env.DISCORD_DEFAULT_CATEGORY,
+      prependRoomName: true
+    }
+  }
 
   // Abort if room doesn't have any guild configured
   if (!guildSettings) {
-    console.log(task.id, task.room, "has no guild settings")
+    console.log(task.id, task.room, "was unable to fetch guild settings")
     return
   }
 
@@ -54,14 +64,14 @@ const onNewTask = async (task) => {
   }
 
   // Get the channels
-  console.log(guildSettings)
   const guild = client.guilds.cache.get(guildSettings.guildId)
   const announcementsChannel = await guild.channels.fetch(guildSettings.announcementsChannelId)
   const newsroomCategoryChannel = await guild.channels.fetch(guildSettings.newsroomCategoryChannelId)
 
   // Create a task channel
-  const taskSlug = slugify(task.title)
-  const taskChannel = await newsroomCategoryChannel.createChannel(taskSlug)
+  const taskSlug = slugify(task.title).toLowerCase()
+  const taskChannelName = `${guildSettings.prependRoomName ? `${task.room}-` : ''}${taskSlug}`
+  const taskChannel = await newsroomCategoryChannel.createChannel(taskChannelName)
 
   // Create the task annoucement
   const taskLink = `https://newsroom.xyz/rooms/${task.room}/${task.id}`
@@ -76,6 +86,7 @@ Interested? Send a gm in <#${taskChannel.id}>
   const db = getFirestore()
   const taskRef = db.collection("tasks").doc(task.id);
   await taskRef.update({
+    discordChannelName: taskChannelName,
     discordInviteCode: invite.code,
   });
 }
@@ -100,7 +111,6 @@ const onRoomConnection = async (room) => {
   const annoucementsChannel = await newsroomCategory.createChannel("announcements", { type: Constants.ChannelTypes.GUILD_TEXT })
   console.log(room.id, "create annoucements channel", annoucementsChannel.id)
 
-
   // Save the channel ids
   const db = getFirestore()
   const roomRef = db.collection('rooms').doc(room.id);
@@ -111,9 +121,11 @@ const onRoomConnection = async (room) => {
 }
 
 const listenForNewTasks = () => {
+  const start = new Date('Nov 20, 2021 00:00:01 GMT+00:00')
   const db = getFirestore();
   db
     .collection('tasks')
+    .where('created', '>', start)
     .onSnapshot((querySnapshot) => {
     querySnapshot.docChanges().forEach(change => {
       if (change.type !== "added") {
